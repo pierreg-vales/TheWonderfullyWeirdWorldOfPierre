8e60abb4db92d648ea5cdc097393c81c57fd5b89
pierreg-vales <pierre.vales@upr.edu>
2026-03-28 19:49:43 -0400

    Add About section

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,14 @@
+import "./About.css"
+
+function About() {
+    return (
+        <section className="about">
+            <h2>About</h2>
+            <p>
+                I build things because it makes me feel free. Everything I create is an extension of myself — and in the process, I build more of myself into the world. I create best when I'm fully immersed, when it stops feeling like work and starts feeling like a performance.
+            </p>
+        </section>
+    )
+}
+
+export default About
